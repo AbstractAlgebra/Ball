884bc9ca2038ba388ee80c82a35772cd8ef939a0
Dallas <Dallas>
2017-02-06 14:08:24 -0800

    refactored code

---
+++ Assets/Scripts/PlayerBoost.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+
+public class PlayerBoost : MonoBehaviour {
+    private float fillAmount;
+    int i = 100;
+
+    [SerializeField]
+    private Image content;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleBar();
+	}
+
+    private void HandleBar()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            i--;
+        }
+        content.fillAmount = Map(i,0,100,0,1);
+    }
+
+    private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+}
---
+++ Assets/Scripts/ResetGame.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        BallMovement ball1 = (BallMovement)GameObject.Find("BlueBall").GetComponent(typeof(BallMovement));
+        BallMovement ball2 = (BallMovement)GameObject.Find("RedBall").GetComponent(typeof(BallMovement));
+        if (ball1.transform.position.y < -5 || ball2.transform.position.y < -5)
+        {
+            resetGame();
+        }
+    
+    }
+
+    void resetGame()
+    {
+        BallMovement ball1 = (BallMovement)GameObject.Find("BlueBall").GetComponent(typeof(BallMovement));
+        BallMovement ball2 = (BallMovement)GameObject.Find("RedBall").GetComponent(typeof(BallMovement));
+        ball1.transform.position = new Vector3(32.2f, 8f, -2f);
+            ball2.transform.position = new Vector3(-49.5f, 9.7f, -2f);
+        ball1.speed = new Vector3(0,0,0);
+        ball2.speed = new Vector3(0,0,0);
+        ball1.acceleration = 0;
+        ball2.acceleration = 0;
+        print("resettingGame");
+        Application.LoadLevel(0);
+    }
+}
